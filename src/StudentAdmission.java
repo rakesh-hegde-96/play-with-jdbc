91c06d61dff6c677df50f1439b294810a379e81e
Rakesh Hegde <rakeshhegde94@gmail.com>
2020-08-23 13:05:31 +0530

    Comment

--- src/StudentAdmission.java
+++ src/StudentAdmission.java
@@ -22,24 +22,24 @@ public class StudentAdmission {
 		String connectionString = "jdbc:mysql://localhost:3306/" + databaseName;
 		Connection connection = DriverManager.getConnection(connectionString, "root", "test");
 		Statement statement = connection.createStatement();
-		P("**Enter Admission Details");
-		P("Name : ");
-		String name = sc.nextLine();
-		P("Score(CGPA/%) *Write only decimals");
-		double score = sc.nextDouble();
-		P("Gender(M/F)");
-		String gender = sc.nextLine();
-		P("Previous School");
-		String prevSchool = sc.nextLine();
-		P("Board : ");
-		String board = sc.nextLine();
-		P("Combination 1-PCMS\n2-PCMCs\n3-PCMB\n4-BASE\n5-BAECs");
-		int combination = sc.nextInt();
-		String command = "INSERT INTO admission (StdentName, Score, Gender, PreviousSchool, Board, combinationId) VALUES"
-				+ String.format("('%s', %.2f, '%s', '%s', '%s', %d)", name, score, gender, prevSchool, board, combination);
-		P("Command "+ command);
+//		P("**Enter Admission Details");
+//		P("Name : ");
+//		String name = sc.nextLine();
+//		P("Score(CGPA/%) *Write only decimals");
+//		double score = sc.nextDouble();
+//		P("Gender(M/F)");
+//		String gender = sc.nextLine();
+//		P("Previous School");
+//		String prevSchool = sc.nextLine();
+//		P("Board : ");
+//		String board = sc.nextLine();
+//		P("Combination 1-PCMS\n2-PCMCs\n3-PCMB\n4-BASE\n5-BAECs");
+//		int combination = sc.nextInt();
+//		String command = "INSERT INTO admission (StdentName, Score, Gender, PreviousSchool, Board, combinationId) VALUES"
+//				+ String.format("('%s', %.2f, '%s', '%s', '%s', %d)", name, score, gender, prevSchool, board, combination);
+//		P("Command "+ command);
 		try {
-			statement.executeUpdate(command);
+			statement.executeUpdate("INSERT INTO admission (StdentName, Score, Gender, PreviousSchool, Board, combinationId) VALUES('Kothyappa', 77.98, 'M', 'AVKF', 'State', 3)");
 			P("Data saved successfully");
 		} catch (Exception e) {
 			// TODO: handle exception
